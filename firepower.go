72559c1dfd7600acf57eb7c2f3622841778fdede
gmarsal <gustavo.andres.marsal@gmail.com>
2019-11-03 11:01:28 -0300

    Version con 2 funciones

--- firepower.go
+++ firepower.go
@@ -3,10 +3,12 @@ package firepower
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -18,9 +20,8 @@ import (
 )
 
 const (
-	defaultBaseURL = "https://ec2-3-90-107-206.compute-1.amazonaws.com/"
+	defaultBaseURL = "https://ec2-3-90-107-206.compute-1.amazonaws.com/api/fmc_config/v1/domain/"
 	defaultAuthURL = "https://ec2-3-90-107-206.compute-1.amazonaws.com/api/fmc_platform/v1/auth/generatetoken"
-	userAgent      = "go-github"
 
 	headerAuthToken  = "X-auth-access-token"
 	headerDomainUUID = "DOMAIN_UUID"
@@ -35,14 +36,11 @@ type Client struct {
 	// set to a domain endpoint to use with GitHub Enterprise. BaseURL should
 	// always be specified with a trailing slash.
 	BaseURL *url.URL
+	common  service // Reuse a single struct instead of allocating one for each service on the heap.
+	token   string  // Auth token
+	domain  string  // Domain UUID
 
-	// User agent used when communicating with the GitHub API.
-	UserAgent string
-
-	rateMu sync.Mutex
-	common service // Reuse a single struct instead of allocating one for each service on the heap.
-
-	// Services used for talking to different parts of the GitHub API.
+	// Services used for talking to different parts of the FIrePower API.
 	Object *ObjectService
 }
 
@@ -98,7 +96,7 @@ func addOptions(s string, opt interface{}) (string, error) {
 	return u.String(), nil
 }
 
-// NewClient returns a new GitHub API client. If a nil httpClient is
+// NewClient returns a new FIrePower API client. If a nil httpClient is
 // provided, a new http.Client will be used. To use API methods which require
 // authentication, provide an http.Client that will perform the authentication
 // for you (such as that provided by the golang.org/x/oauth2 library).
@@ -106,9 +104,12 @@ func NewClient(httpClient *http.Client) *Client {
 	if httpClient == nil {
 		httpClient = &http.Client{}
 	}
+
+	t, d := GetAuthData(httpClient)
+
 	baseURL, _ := url.Parse(defaultBaseURL)
 
-	c := &Client{client: httpClient, BaseURL: baseURL, UserAgent: userAgent}
+	c := &Client{client: httpClient, BaseURL: baseURL, token: t, domain: d}
 	c.common.client = c
 	c.Object = (*ObjectService)(&c.common)
 
@@ -124,7 +125,8 @@ func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Requ
 	if !strings.HasSuffix(c.BaseURL.Path, "/") {
 		return nil, fmt.Errorf("BaseURL must have a trailing slash, but %q does not", c.BaseURL)
 	}
-	u, err := c.BaseURL.Parse(urlStr)
+	u, err := c.BaseURL.Parse(c.domain + "/" + urlStr)
+
 	if err != nil {
 		return nil, err
 	}
@@ -149,9 +151,9 @@ func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Requ
 		req.Header.Set("Content-Type", "application/json")
 	}
 
-	if c.UserAgent != "" {
-		req.Header.Set("User-Agent", c.UserAgent)
-	}
+	//Auth
+	req.Header.Set("X-auth-access-token", c.token)
+
 	return req, nil
 }
 
@@ -176,7 +178,7 @@ type Response struct {
 // r must not be nil.
 func newResponse(r *http.Response) *Response {
 	response := &Response{Response: r}
-	response.populatePageValues()
+	//response.populatePageValues()
 	return response
 }
 
@@ -225,13 +227,10 @@ func (r *Response) populatePageValues() {
 }
 
 // Do sends an API request and returns the API response. The API response is
-// JSON decoded and stored in the value pointed to by v, or returned as an
+// JSON decoded and stored in the value pointe	d to by v, or returned as an
 // error if an API error has occurred. If v implements the io.Writer
 // interface, the raw response body will be written to v, without attempting to
 // first decode it. If rate limit is exceeded and reset time is in the future,
-
-// The provided ctx must be non-nil. If it is canceled or times out,
-// ctx.Err() will be returned.
 func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*Response, error) {
 	req = withContext(ctx, req)
 
@@ -284,7 +283,8 @@ func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*Res
 		if w, ok := v.(io.Writer); ok {
 			io.Copy(w, resp.Body)
 		} else {
-			decErr := json.NewDecoder(resp.Body).Decode(v)
+			decErr := json.NewDecoder(resp.Body).Decode(&v)
+
 			if decErr == io.EOF {
 				decErr = nil // ignore EOF errors caused by empty response body
 			}
@@ -379,4 +379,26 @@ type AcceptedError struct {
 
 func (*AcceptedError) Error() string {
 	return "job scheduled on GitHub side; try again later"
+}
+
+// GetAuthData a
+func GetAuthData(httpClient *http.Client) (string, string) {
+
+	if httpClient == nil {
+		httpClient = &http.Client{}
+	}
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	req, err := http.NewRequest("POST", defaultAuthURL, nil)
+	req.SetBasicAuth("admin", "HY2M*wQH6I#xkY")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	token := resp.Header.Get("X-auth-access-token")
+	domain := resp.Header.Get("DOMAIN_UUID")
+
+	return token, domain
 }
--- object.go
+++ object.go
@@ -5,77 +5,65 @@ import (
 )
 
 // ObjectService handles communication with the object related methods of the
-// GitHub API docs: https://developer.github.com/v3/enterprise/
 type ObjectService service
 
-// Object represents a GitHub issue on a repository.
-//
-// Note: As far as the GitHub API is concerned, every pull request is an issue,
-// but not every issue is a pull request. Some endpoints, events, and webhooks
-// may also return pull requests via this struct. If PullRequestLinks is nil,
-// this is an issue, and if PullRequestLinks is not nil, this is a pull request.
-// The IsPullRequest helper method can be used to check that.
+// Object represents a FirePower object
 type Object struct {
-	Links struct {
-		Self   string `json:"self"`
-		Parent string `json:"parent"`
-	} `json:"links"`
-	Type        string `json:"type"`
-	Value       string `json:"value"`
-	Overridable bool   `json:"overridable"`
+	Links  Links  `json:"links"`
+	Items  []Item `json:"items"`
+	Paging Paging `json:"paging"`
+}
+
+type Links struct {
+	Self string `json:"self"`
+}
+
+type Paging struct {
+	Count  int `json:"count"`
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
+	Pages  int `json:"pages"`
+}
+
+type Item struct {
 	Description string `json:"description"`
 	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Metadata    struct {
-		Timestamp int64 `json:"timestamp"`
-		LastUser  struct {
-			Name string `json:"name"`
-		} `json:"lastUser"`
+	Links       struct {
+		Parent string `json:"parent"`
+		Self   string `json:"self"`
+	} `json:"links"`
+	Metadata struct {
 		Domain struct {
-			Name string `json:"name"`
 			ID   string `json:"id"`
+			Name string `json:"name"`
 			Type string `json:"type"`
 		} `json:"domain"`
-		IPType     string `json:"ipType"`
+		IPType   string `json:"ipType"`
+		LastUser struct {
+			Name string `json:"name"`
+		} `json:"lastUser"`
 		ParentType string `json:"parentType"`
+		ReadOnly   struct {
+			Reason string `json:"reason"`
+			State  bool   `json:"state"`
+		} `json:"readOnly"`
+		Timestamp int `json:"timestamp"`
 	} `json:"metadata"`
+	Name        string `json:"name"`
+	Overridable bool   `json:"overridable"`
+	Type        string `json:"type"`
+	Value       string `json:"value"`
 }
 
-// IssueRequest represents a request to create/edit an issue.
-// It is separate from Issue above because otherwise Labels
-// and Assignee fail to serialize to the correct JSON.
-type IssueRequest struct {
-	Title     *string   `json:"title,omitempty"`
-	Body      *string   `json:"body,omitempty"`
-	Labels    *[]string `json:"labels,omitempty"`
-	Assignee  *string   `json:"assignee,omitempty"`
-	State     *string   `json:"state,omitempty"`
-	Milestone *int      `json:"milestone,omitempty"`
-	Assignees *[]string `json:"assignees,omitempty"`
-}
-
-// IssueListOptions specifies the optional parameters to the IssuesService.List
+// ObjectListOptions specifies the optional parameters to the IssuesService.List
 // and IssuesService.ListByOrg methods.
 type ObjectListOptions struct {
-	// Filter specifies which issues to list. Possible values are: assigned,
+	// Expanded specifies which issues to list. Possible values are: assigned,
 	// created, mentioned, subscribed, all. Default is "assigned".
-	Filter string `url:"limit,omitempty"`
-
-	ListOptions
+	Expanded bool `url:"expanded,true"`
 }
 
-// List the Objects for the authenticated user. If all is true, list issues
-func (s *ObjectService) List(ctx context.Context, all bool, opt *ObjectListOptions) ([]*Object, *Response, error) {
-	var u string
-	if all {
-		u = "issues"
-	} else {
-		u = "user/issues"
-	}
-	return s.listObjects(ctx, u, opt)
-}
-
-func (s *ObjectService) listObjects(ctx context.Context, u string, opt *ObjectListOptions) ([]*Object, *Response, error) {
+func (s *ObjectService) listObjects(ctx context.Context, u string, opt *ObjectListOptions) (*Object, *Response, error) {
 	u, err := addOptions(u, opt)
 	if err != nil {
 		return nil, nil, err
@@ -86,7 +74,7 @@ func (s *ObjectService) listObjects(ctx context.Context, u string, opt *ObjectLi
 		return nil, nil, err
 	}
 
-	var objects []*Object
+	var objects *Object
 	resp, err := s.client.Do(ctx, req, &objects)
 	if err != nil {
 		return nil, resp, err
@@ -95,11 +83,48 @@ func (s *ObjectService) listObjects(ctx context.Context, u string, opt *ObjectLi
 	return objects, resp, nil
 }
 
-type ObjectRequest struct {
-	Type        string `json:"type"`
-	Value       string `json:"value"`
-	Overridable bool   `json:"overridable"`
-	Description string `json:"description"`
-	ID          string `json:"id"`
-	Name        string `json:"name"`
+func (s *ObjectService) getObject(ctx context.Context, u string, opt *ObjectListOptions) (*Item, *Response, error) {
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var item *Item
+	resp, err := s.client.Do(ctx, req, &item)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return item, resp, nil
+}
+
+//InsertObject Insert
+func (s *ObjectService) InsertObject(ctx context.Context, u string, i Item, opt *ObjectListOptions) (*Response, error) {
+	req, err := s.client.NewRequest("POST", u, i)
+	if err != nil {
+		return nil, err
+	}
+
+	var item *Item
+	resp, err := s.client.Do(ctx, req, &item)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// ListHosts the Objects for the authenticated user.
+func (s *ObjectService) ListHosts(ctx context.Context, opt *ObjectListOptions) (*Object, *Response, error) {
+	return s.listObjects(ctx, "object/hosts", opt)
+}
+
+// GetHost the Objects for the authenticated user.
+func (s *ObjectService) GetHost(ctx context.Context, hostid string, opt *ObjectListOptions) (*Item, *Response, error) {
+	return s.getObject(ctx, "object/hosts/"+hostid, opt)
+}
+
+// InsertHost the Objects for the authenticated user.
+func (s *ObjectService) InsertHost(ctx context.Context, i Item) (*Response, error) {
+	return s.InsertObject(ctx, "object/hosts/", i, nil)
 }
